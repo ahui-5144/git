399a2e7c619950b663baccbf94f2f51edcfa9c9d
ahui <ye@qq.com>
2021-01-25 20:20:52 +0800

    ahui's first git in Idea

---
+++ src/main/java/com/ahui/git/Apple.java
@@ -0,0 +1,8 @@
+package com.ahui.git;
+
+/**
+ * @author AHui
+ * @create 2021-01-25 19:26
+ */
+public class Apple {
+}
---
+++ src/test/java/com/ahui/test/GoodTest.java
@@ -0,0 +1,8 @@
+package com.ahui.test;
+
+/**
+ * @author AHui
+ * @create 2021-01-25 19:27
+ */
+public class GoodTest {
+}
